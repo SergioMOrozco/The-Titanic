976bc718be9c9cc0cace69632893baa1cad05e50
Sergio Orozco <sorozco0612@yahoo.com>
2019-12-12 20:07:03 -0700

    Saved SGD best model

--- sgd_model_creation.py
+++ sgd_model_creation.py
@@ -1,5 +1,7 @@
 from sklearn.linear_model import SGDClassifier
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.externals import joblib
+from scipy.stats import expon, reciprocal
 import TitanicPipeline
 import pandas as pd
 import numpy as np
@@ -17,14 +19,18 @@ y_train = preprocessed_titanic_data_df["Survived"]
 sgd_clf = SGDClassifier(random_state=42,)
 
 grid_param = {
-    'loss' : ['hinge','modified_huber','log'],
-    'penalty' : ['l1','l2','elasticnet']
+    'loss' : ['hinge','modified_huber','log','squared_hinge','perceptron'],
+    'penalty' : ['l1','l2','elasticnet'],
+    'learning_rate' : ['constant','optimal','invscaling','adaptive'],
+    'eta0' : expon(scale=4)
 }
 
-gd_sr = GridSearchCV(sgd_clf,grid_param,scoring='accuracy',cv=5,n_jobs=8)
+gd_sgd = RandomizedSearchCV(sgd_clf,grid_param,scoring='accuracy',cv=5,n_jobs=8)
 
-gd_sr.fit(X_train,y_train)
+gd_sgd.fit(X_train,y_train)
 
-print(gd_sr.best_params_)
-print(gd_sr.best_score_)
+print(gd_sgd.best_params_)
+print(gd_sgd.best_score_)
+
+joblib.dump(gd_sgd, "sgd_classifier.pkl")
 
